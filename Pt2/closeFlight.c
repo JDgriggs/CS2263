d2801cd7e2b992090b620d53ed2739dc70046dd3
JDgriggs <32017004+JDgriggs@users.noreply.github.com>
2019-10-03 19:07:52 -0300

    Assignment 2
    Finished parts 2 and 3

--- Pt2/closeFlight.c
+++ Pt2/closeFlight.c
@@ -9,21 +9,93 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void findClosestFlight(int desiredTime, int *departureTime, int *arrivalTime){
+int findClosestFlight(int desiredTime, int *departureTime, int *arrivalTime){
 	
 	FILE *f = fopen("flights.dat", "r");
 	
 	int hours = 0;
 	int mins = 0;
 	
+	int check = fscanf(f,"%d:%d", &hours, &mins);
+	//printf("\n%02d:%02d\n", hours, mins);//test code
 	
+	if(check == 2){
+		*departureTime = mins + (hours*100);
+		check = fscanf(f,"%d:%d", &hours, &mins);
+		if(check == 2){
+			*arrivalTime = mins + (hours*100);
+		}
+		else
+		{
+			printf("error1");
+			return 2;
+		}
+	}
+	else{
+		printf("error2");
+		return 2;
+	}
+	
+	
+	while(check ==2){
+		if(desiredTime <= *departureTime){
+			return 1;
+		}
+		else{
+			check = fscanf(f,"%d:%d", &hours, &mins);
+	
+			if(check == 2){
+				*departureTime = mins + (hours*100);
+				check = fscanf(f,"%d:%d", &hours, &mins);
+				if(check == 2){
+					*arrivalTime = mins + (hours*100);
+				}
+				else
+				{
+					printf("error3");
+					return 2;
+				}
+			}
+		}
+	}
+	
+	return 0;
 }
 
 
 int main(){
 	
-	int hours = 00;
-	int mins = 00;
+	int hours = 0;
+	int mins = 0;
+	int desiredTime = 0;
+	int *departureTime = malloc(sizeof(int));
+	int *arrivalTime = malloc(sizeof(int));
 	
-	printf("%d : %d\n", hours, mins);
+	printf("Enter a 24-hour time: ");
+	scanf("%d:%d", &hours, &mins);
+	
+	desiredTime = mins + (hours*100);
+	*departureTime = 0;
+	*arrivalTime = 0;
+	
+	//For testing
+	mins = 0;
+	hours = 0;
+	
+	//printf("%2d", desiredTime);// testing
+	
+	int check = findClosestFlight(desiredTime, departureTime,arrivalTime);
+	
+	
+	if(check == 1){
+		mins = *departureTime % 100;
+		hours = (*departureTime - mins)/100;
+		printf("\nClosest Departure time is %02d:%02d,", hours, mins);
+		mins = *arrivalTime % 100;
+		hours = (*arrivalTime - mins)/100;
+		printf(" arriving at %02d:%02d\n", hours, mins);
+	}
+	else if(check == 0){
+		printf("Sorry, No avaiable departures\n");
+	}
 }
